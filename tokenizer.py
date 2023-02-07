e7eacac42aa231bc584466f9d4542fc128b54e5e
michael-gif <mwxfranco@gmail.com>
2023-02-07 17:49:55 +0000

    replaced excess logic with a dictionary of parsers and a for loop

--- tokenizer.py
+++ tokenizer.py
@@ -325,6 +325,14 @@ def tokenize(content: str) -> List[Token]:
     :return:
     """
     tokens = []
+    parsers = {
+        'reusable ': function_parser,
+        'call ': call_parser,
+        'static ': static_parser,
+        'repeat ': repeat_query_parser,
+        'query ': query_parser
+    }
+    parser_keys = list(parsers.keys())
 
     # parse text
     read = ""
@@ -392,51 +400,61 @@ def tokenize(content: str) -> List[Token]:
             index += 1
             continue
 
-        if read.startswith("reusable "):
-            # print("reusable " + content[index + 1:])
-            text, token = function_parser("reusable " + content[index + 1:])
-            if token:
-                content = text
-                tokens.append(token)
-                read = ""
-                index = 0
-                continue
+        for key in parser_keys:
+            if read.startswith(key):
+                text, token = parsers[key](key + content[index + 1:])
+                if token:
+                    content = text
+                    tokens.append(token)
+                    read = ""
+                    index = 0
+                    continue
 
-        if read.startswith("call "):
-            text, token = call_parser("call " + content[index + 1:])
-            if token:
-                content = text
-                tokens.append(token)
-                read = ""
-                index = 0
-                continue
-
-        if read.startswith("static "):
-            text, token = static_parser("static " + content[index + 1:])
-            if token:
-                content = text
-                tokens.append(token)
-                read = ""
-                index = 0
-                continue
-
-        if read.startswith("repeat "):
-            text, token = repeat_query_parser("repeat " + content[index + 1:])
-            if token:
-                content = text
-                tokens.append(token)
-                read = ""
-                index = 0
-                continue
-
-        if read.startswith("query "):
-            text, token = query_parser(content)
-            if token:
-                content = text
-                tokens.append(token)
-                read = ""
-                index = 0
-                continue
+        # if read.startswith("reusable "):
+        #     # print("reusable " + content[index + 1:])
+        #     text, token = function_parser("reusable " + content[index + 1:])
+        #     if token:
+        #         content = text
+        #         tokens.append(token)
+        #         read = ""
+        #         index = 0
+        #         continue
+        #
+        # if read.startswith("call "):
+        #     text, token = call_parser("call " + content[index + 1:])
+        #     if token:
+        #         content = text
+        #         tokens.append(token)
+        #         read = ""
+        #         index = 0
+        #         continue
+        #
+        # if read.startswith("static "):
+        #     text, token = static_parser("static " + content[index + 1:])
+        #     if token:
+        #         content = text
+        #         tokens.append(token)
+        #         read = ""
+        #         index = 0
+        #         continue
+        #
+        # if read.startswith("repeat "):
+        #     text, token = repeat_query_parser("repeat " + content[index + 1:])
+        #     if token:
+        #         content = text
+        #         tokens.append(token)
+        #         read = ""
+        #         index = 0
+        #         continue
+        #
+        # if read.startswith("query "):
+        #     text, token = query_parser(content)
+        #     if token:
+        #         content = text
+        #         tokens.append(token)
+        #         read = ""
+        #         index = 0
+        #         continue
 
         index += 1
 
