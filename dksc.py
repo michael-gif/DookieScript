80cbe4a44d7a90bfe643ae4cdd6b4e94d0cc03c2
John <mwxfranco@gmail.com>
2023-02-08 14:46:47 +0000

    added -out compiler option

--- dksc.py
+++ dksc.py
@@ -5,6 +5,7 @@ from tokenizer import tokenize
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-dks', '--dks_file', required=True)
+parser.add_argument('-out', '--out_file')
 args = vars(parser.parse_args())
 
 dks_file = args['dks_file']
@@ -18,6 +19,9 @@ try:
     if not content.endswith("\n"):
         content = content + "\n"
     tokens = tokenize(content)
-    translate_to_python(tokens, dks_file[:-4] + ".py")
+    if args['out_file']:
+        translate_to_python(tokens, args['out_file'])
+    else:
+        translate_to_python(tokens, dks_file[:-4] + ".py")
 except FileNotFoundError:
     print(f"[INFO] Could not find dks file '{dks_file}'")
